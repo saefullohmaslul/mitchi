6a428ac14be249c7c18585dad36a4e9c24b04688
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2024-10-08 23:09:19 +0700

    feat: improvement ui chatbot

--- app.py
+++ app.py
@@ -30,13 +30,28 @@ async def predict(message: str, history: List) -> AsyncGenerator:
 
 
 if __name__ == "__main__":
+    js_func = """
+        function refresh() {
+            const url = new URL(window.location);
+
+            if (url.searchParams.get('__theme') !== 'dark') {
+                url.searchParams.set('__theme', 'dark');
+                window.location.href = url.href;
+            }
+        }
+    """
+
     interface = gr.ChatInterface(
         fn=predict,
-        title="Mitchi: Smart Research Discussion Chatbot",
+        title="Mitchi",
+        description="<p style='text-align: center;'>Smart Research Discussion Chatbot</p>",
         examples=[
             "Halo, kamu siapa?",
             "Aku mau bikin penelitian tapi bingung topiknya apa",
             "Aku mau cari referensi tentang machine learning",
         ],
+        concurrency_limit=1,
+        theme=gr.themes.Default(),
+        js=js_func,
     )
     interface.launch(server_name="0.0.0.0", server_port=7860, share=False)
